bba8dedf3ac684123d548f86d6693dbce91514f9
Andres Munoz <anfe0690@gmail.com>
2023-06-01 18:24:40 -0500

    Added delete functionality

--- src/records/Records.js
+++ src/records/Records.js
@@ -13,33 +13,35 @@ export default function Records() {
   const [loggedIn, setLoggedIn] = useState(true);
   const [pageMessage, setPageMessage] = useState(null);
   const [records, setRecords] = useState([]);
-  const [recordsPerPage, setRecordsPerPage] = useState(10);
-  const [pageNumber, setPageNumber] = useState(0);
+  const [pageControl, setPageControl] = useState({ recordsPerPage: 10, pageNumber: 0, maxPage: 0 });
   const [sortBy, setSortBy] = useState([]);
   const [search, setSearch] = useState('');
 
   const recordsHtml = records.map(r => (
     <tr key={r.id}>
+      <td>{r.id}</td>
       <td>{r.type}</td>
       <td>{r.amount}</td>
       <td>{r.balance}</td>
       <td>{r.result}</td>
       <td>{r.date}</td>
+      <td className="text-center"><input class="form-check-input" type="checkbox" data-id={r.id}
+          onChange={handleRecordSelected} checked={r.selected}></input></td>
     </tr>
   ))
 
-  const pagesIndexes = Array.from(Array(pageNumber+1).keys());
-  const pagesButtons = pagesIndexes.map(i => <li key={i} className={i === pageNumber ? "page-item active" : "page-item"}>
-      <button className="page-link" data-page={i} onClick={e => setPageNumber(parseInt(e.currentTarget.dataset.page))}>{i+1}</button>
-    </li> );
+  const pagesIndexes = Array.from(Array(pageControl.maxPage+1).keys());
+  const pagesButtons = pagesIndexes.map(i => (<li key={i} className={i === pageControl.pageNumber ? "page-item active" : "page-item"}>
+        <button className="page-link" data-page={i} onClick={handlePageSelection}>{i+1}</button>
+      </li>));
 
   useEffect(() => {
     console.log('Loading records');
     let ignore = false;
 
     let params = new URLSearchParams();
-    params.append('recordsPerPage', recordsPerPage);
-    params.append('page', pageNumber);
+    params.append('recordsPerPage', pageControl.recordsPerPage);
+    params.append('page', pageControl.pageNumber);
     sortBy.forEach(sb => {
       params.append('sortBy', sb.name);
       params.append('sortDirection', sb.direction)
@@ -54,6 +56,7 @@ export default function Records() {
       .then(function (response){
         if (!ignore) {
           console.log(response.data);
+          response.data.forEach(r => r.selected = false);
           setRecords(response.data);
         }
       })
@@ -83,7 +86,7 @@ export default function Records() {
     return () => {
       ignore = true;
     };
-  }, [recordsPerPage, pageNumber, sortBy, search, navigate]);
+  }, [pageControl, sortBy, search, navigate]);
 
   function filterNonNumberCharacters(e) {
     if (e.key.length === 1 && !/[0-9]/.test(e.key)) {
@@ -127,6 +130,86 @@ export default function Records() {
     setSortBy(newSortBy);
   }
 
+  function handlePageSelection(e) {
+    setPageControl(n => ({
+      ...n,
+      pageNumber: parseInt(e.target.dataset.page)
+    }))
+  }
+
+  function handlePrevious() {
+    setPageControl(n => ({
+      ...n,
+      pageNumber: n.pageNumber-1,
+      maxPage: Math.max(n.pageNumber-1, n.maxPage)
+    }));
+  }
+
+  function handleNext() {
+    setPageControl(n => ({
+      ...n,
+      pageNumber: n.pageNumber+1,
+      maxPage: Math.max(n.pageNumber+1, n.maxPage)
+    }));
+  }
+
+  function handleRecordSelected(e) {
+    let id = parseInt(e.currentTarget.dataset.id);
+    console.log('Record selected/unselected: ' + id);
+    let newRecords = [];
+    records.forEach(function(item, index) {
+      let newRecord = Object.assign({}, item);
+      if (item.id === id) {
+        newRecord.selected = !item.selected;
+      }
+      newRecords.push(newRecord);
+    });
+    setRecords(newRecords);
+  }
+
+  function handleDeleteSelected() {
+    console.log('Deleting records');
+    let deletingRecords = records.filter(r => r.selected).map(r => r.id);
+    console.log(deletingRecords);
+
+    axios.delete('records', {
+          data: deletingRecords
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(function (response){
+          const message = 'Operation successful';
+          console.log(message);
+          setPageMessage({ message: message, type: 'success' });
+
+          let keepingRecords = records.filter(r => !r.selected);
+          setRecords(keepingRecords);
+        })
+        .catch(function (error){
+          if (error.response.status === 401) {
+            console.log('Error with session. Redirecting to root.');
+            Cookies.remove('user');
+            navigate('/');
+            return;
+          }
+
+          let errorMessage = 'Error deleting records: ' + error.message
+          if (error.response) {
+            if (error.response.data && error.response.data.error) {
+              errorMessage += ' - ' + error.response.data.error
+            }
+            else {
+              errorMessage += ' - ' + error.response.data
+            }
+          }
+
+          console.log(errorMessage)
+          setPageMessage({ message: errorMessage, type: 'failure' })
+        });
+  }
+
   return (
     <>
       <Helmet>
@@ -145,7 +228,8 @@ export default function Records() {
           </div>
           <div className="col-auto">
             <input id="records-per-page" className="form-control" type="number" onKeyDown={filterNonNumberCharacters}
-                onChange={e => setRecordsPerPage(Math.max(parseInt(e.currentTarget.value), 1))} value={recordsPerPage}></input>
+                onChange={e => setPageControl({ recordsPerPage: Math.max(parseInt(e.currentTarget.value), 1), pageNumber: 0, maxPage: 0 })}
+                value={pageControl.recordsPerPage}></input>
           </div>
         </div>
 
@@ -167,11 +251,15 @@ export default function Records() {
           <table className="table table-bordered table-striped">
             <thead>
               <tr>
+                <th scope="col" role="button" data-name="id" onClick={setSortByAndSortDirection}>ID {calculateCaret('id')}</th>
                 <th scope="col" role="button" data-name="type" onClick={setSortByAndSortDirection}>Operation {calculateCaret('type')}</th>
                 <th scope="col" role="button" data-name="amount" onClick={setSortByAndSortDirection}>Amount {calculateCaret('amount')}</th>
                 <th scope="col" role="button" data-name="userBalance" onClick={setSortByAndSortDirection}>Balance {calculateCaret('userBalance')}</th>
                 <th scope="col" role="button" data-name="operationResponse" onClick={setSortByAndSortDirection}>Result {calculateCaret('operationResponse')}</th>
                 <th scope="col" role="button" data-name="date" onClick={setSortByAndSortDirection}>Date {calculateCaret('date')}</th>
+                <th scope="col" className="text-center" onClick={handleDeleteSelected}>
+                  <button type="button" className={records.find(r => r.selected) ? "btn btn-danger" : "btn btn-danger disabled"}>Delete</button>
+                </th>
               </tr>
             </thead>
             <tbody>{recordsHtml}</tbody>
@@ -181,11 +269,11 @@ export default function Records() {
         <div className="row">
           <nav>
             <ul className="pagination justify-content-center">
-              <li className={pageNumber === 0 ? "page-item disabled": "page-item"}><button className="page-link"
-                  onClick={() => setPageNumber(n => n - 1)}>Previous</button></li>
+              <li className={pageControl.pageNumber === 0 ? "page-item disabled": "page-item"}><button className="page-link"
+                  onClick={handlePrevious}>Previous</button></li>
               {pagesButtons}
-              <li className={records.length < recordsPerPage ? "page-item disabled": "page-item"}><button className="page-link"
-                  onClick={() => setPageNumber(n => n + 1)}>Next</button></li>
+              <li className={records.length < pageControl.recordsPerPage ? "page-item disabled": "page-item"}><button className="page-link"
+                  onClick={handleNext}>Next</button></li>
             </ul>
           </nav>
         </div>
